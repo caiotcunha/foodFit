3ca01b4b64b71d8bf5d7976139ab5bc24087fe49
IvanAssis07 <ivan.assis07@gmail.com>
2023-04-12 15:27:01 -0300

    fix:colocado a porta para rodar a aplicação

--- api/index.js
+++ api/index.js
@@ -5,8 +5,10 @@ const User = require('./src/domains/users/models/User');
 
 
 app.get('/', async(req, res,next) => {
-    await sequelize.sync
+    await sequelize.sync()
 })
+
+// Se você não comentar, vai resetar o banco toda vez que o app rodar
 // sequelize.sync();
 
-app.listen(console.log(sequelize.sync()));
+app.listen(process.env.PORT, console.log('Server running in port 3030'));
