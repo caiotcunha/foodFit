9ad4acedec3954f60e2e9583af5c7b80226b20f4
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-04-23 12:56:58 -0300

    feat: :sparkles: Página de dietas
    Inserindo página de dietas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 import RoutesApp from './routes'
 import LoginPage from './pages/loginPage'
---
+++ client/src/pages/dietPage/index.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import "./style.css"
+
+export default function DietPage() {
+    return (
+        <>
+
+        </>
+    );
+}
