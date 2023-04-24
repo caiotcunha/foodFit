18b9dbc03b3aba5b79f92097239ea891d91b4cfa
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-04-24 11:53:55 -0300

    feat: :sparkles: Iniciando desenvolvimento da tela de dietas

--- client/src/pages/dietPage/index.jsx
+++ client/src/pages/dietPage/index.jsx
@@ -1,10 +1,29 @@
 import React, { useEffect, useState } from "react";
+
+import { FaRegUserCircle } from "react-icons/fa";
+import { AiOutlineMenu } from "react-icons/ai";
+
 import "./style.css"
+import Logo from "../../assets/logo_semfundo.png"
 
 export default function DietPage() {
     return (
         <>
-            
+            <div className="top">
+                <img src={Logo} alt="logo foodfit" className="logo" />
+                <div className="icons">
+                    <div className="user">
+                        <FaRegUserCircle class="icon"/>
+                    </div>
+                    <div className="menu">
+                        <AiOutlineMenu class="icon"/>
+                    </div>
+                </div>
+
+            </div>
+            <div className="bottom">
+
+            </div>
         </>
     );
 }
